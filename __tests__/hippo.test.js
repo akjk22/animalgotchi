722c051929eca254c0bf2fe12fa9884e1c3044cc
Andrew Kim <andrewswkim47@gmail.com>
2020-03-08 12:53:12 -0700

    Create test for hungerLevel function.

--- __tests__/hippo.test.js
+++ __tests__/hippo.test.js
@@ -6,7 +6,7 @@ describe('Daisy', () => {
 
   beforeEach(function() {
     daisy = new Hippo('Daisy');
-    // daisy.setHunger();
+    daisy.setHunger();
     daisy.setHealth();
   });
 
@@ -19,9 +19,14 @@ describe('Daisy', () => {
     expect(daisy.healthLevel).toEqual(100);
   });
 
-  test(`should show health decreasing by 1 every second`, () => {
-    jest.advanceTimersByTime(1001);
+  test(`should show health decreasing by 1 every minute`, () => {
+    jest.advanceTimersByTime(60001);
     expect(daisy.healthLevel).toEqual(99);
+  });
+
+  test(`should have hungerLevel that increases every 20 seconds`, () => {
+    jest.advanceTimersByTime(20001);
+    expect(daisy.hungerLevel).toEqual(11);
   });
   // test('should have a food level of 7 after 3001 milliseconds', () => {
   //   jest.advanceTimersByTime(3001);
--- src/hippo.js
+++ src/hippo.js
@@ -2,20 +2,27 @@ export class Hippo {
   constructor(name) {
     this.name = name;
     this.healthLevel = 100;
-  }
-  setHunger() {
-    setInterval(() => {
-      this.foodLevel--;
-    }, 1000);
+    this.hungerLevel = 10;
   }
 
-  didYouGetSick() {
-    if (this.foodLevel > 0) {
-      return false;
-    } else {
-      return true;
-    }
+  setHealth() {
+    setInterval(() => {
+      this.healthLevel -=1;
+    }, 60000);
   }
+
+  setHunger() {
+    setInterval(() => {
+      this.hungerLevel += 1;
+    }, 20000);
+  }
+  // didYouGetSick() {
+  //   if (this.foodLevel > 0) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
 
   feed() {
     this.foodLevel = 10;
