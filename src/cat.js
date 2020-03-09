15322743c847ddd5b478d28f007939f77da60664
wrunion <winterrunion@gmail.com>
2020-03-09 15:00:07 -0700

    Add feedCount property and increment it by 1 every time cat is fed.

--- src/cat.js
+++ src/cat.js
@@ -1,26 +1,31 @@
 export class Cat {
   constructor(name) {
     this.name = name;
+    this.energy = 100;
     this.healthLevel = 100;
     this.hunger = 0;
-    this.energy = 100;
+    this.feedCount = 0;
     this.hasPooped = false;
   }
 
+  //Every 20 seconds, hunger method increases by 1
   increaseHunger() {
     setInterval(() => {
       this.hunger += 1;
     }, 20000);
   }
 
+  //Every 10 seconds, energy is decreased by 1. 
   decreaseEnergy() {
     setInterval(() => {
       this.energy -= 1;
     }, 10000);
   }
   
+  //User triggered methods: 
   feed() {
     this.hunger -= 1;
     this.energy += 10;
+    this.feedCount += 1;
   }
 }
