14cad201a683a6320b78789eedf52c7a92a7aa12
wrunion <winterrunion@gmail.com>
2020-03-09 14:15:32 -0700

    Create test for various cat properties with default values.

--- __tests__/cat.test.js
+++ __tests__/cat.test.js
@@ -16,5 +16,11 @@ describe('Cat', () => {
   test(`should have a name and health level of 100 when created`, () => {
     expect(jerome.name).toEqual('Jerome');
     expect(jerome.healthLevel).toEqual(100);
-  })
+  });
+
+  test(`should have hunger, hasPooped, and energy properties, each with a default value`, () => {
+    expect(jerome.hunger).toEqual(10);
+    expect(jerome.hasPooped).toEqual(false);
+    expect(jerome.energy).toEqual(100);
+  });
 });
