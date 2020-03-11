a3853a08bd3e9f5592a8b921840454d639b00e3c
wrunion <winterrunion@gmail.com>
2020-03-10 20:35:54 -0700

    Create feed Jerry div.

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import $ from 'jquery';
 import './styles.css';
 
 // eslint-disable-next-line no-unused-vars
-const hippo = new Animal("Jerry", "hippo");
+const jerry = new Animal("Jerry", "hippo");
 
 $(document).ready(function() {
 
+  $("#hunger-level").text(jerry.hunger);
   $("button#feedJerry").click(function() {
-
-
-    
+    jerry.feed();
+    $("#hunger-level").text(jerry.hunger);
+    console.log('Jerry has been fed');
+    console.log(jerry.hunger);
   });  
 
 });
